0f219fa7aa2b26c221663825e1bf9838f0bea3f4
Angela Prestano <angiprestano99@gmail.com>
2024-01-26 12:23:15 +0100

    add getter, setter, tostring

--- src/main/java/angiprestano/organization_event/entities/AdminEvent.java
+++ src/main/java/angiprestano/organization_event/entities/AdminEvent.java
@@ -1,7 +1,14 @@
 package angiprestano.organization_event.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.UUID;
 
+@Getter
+@Setter
+@ToString
 public class AdminEvent {
     private UUID id;
     private String name;
