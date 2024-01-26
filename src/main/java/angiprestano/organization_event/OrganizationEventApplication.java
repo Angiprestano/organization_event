2763c32062110274dd0651088dfe4d5876d971d4
Angela Prestano <angiprestano99@gmail.com>
2024-01-26 10:42:47 +0100

    Initial commit

---
+++ src/main/java/angiprestano/organization_event/OrganizationEventApplication.java
@@ -0,0 +1,12 @@
+package angiprestano.organization_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationEventApplication.class, args);
+	}
+}
---
+++ src/test/java/angiprestano/organization_event/OrganizationEventApplicationTests.java
@@ -0,0 +1,13 @@
+package angiprestano.organization_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
